947de0151fc2a91002d0355c42f57f202dbe2219
Rathin Mondal <116378963+Rathin8670@users.noreply.github.com>
2024-12-07 20:00:16 +0530

    added feed api

--- BE/src/routes/user.js
+++ BE/src/routes/user.js
@@ -55,17 +55,54 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
 
 // get all users 
 userRouter.get('/user/feed',userAuth,async(req,res)=>{
-    try{
-        const user=await User.find({
+    const loggedInUser=req.user;
+    
+    // pagination logic
+    const page=parseInt(req.query.page)|| 1;
+    let limit=parseInt(req.query.limit) || 10;
+    // sanitize limit data
+    limit=limit>50?50:limit;
+    const skip=(page-1)*limit;
 
+    try{
+        /**
+         * 0. Users own profile is not allowed
+         * 1. The profiles whom are sent by user 
+         *  & the fellow users who sent to User are also not allowed.
+         * 2. The people whom are rejected and ignored are also not allowed
+         */
+        const connectedUser=await ConnectionRequest.find({
+            $or:[
+                {fromUserId:loggedInUser._id},
+                {toUserId:loggedInUser._id}
+                ]
         })
-        if(!user){
-            res.status(400).send("something wrong bro")
-        }else{
-            res.send(user)
-        }  
+        .select("fromUserId toUserId")  // the field i want to see
+        .populate("fromUserId","firstName lastName") 
+        .populate("toUserId","firstName lastName")
+
+        const hideUserFromFeed=new Set();
+        connectedUser.forEach((key)=>{
+            hideUserFromFeed.add(key.fromUserId._id.toString())
+            hideUserFromFeed.add(key.toUserId._id.toString())
+        })
+
+        // $nin -> not in the array
+        // $ne ->not equal
+
+        const users=await User.find({
+            $and:[
+                {_id:{ $nin: Array.from(hideUserFromFeed)}},
+                {_id:{$ne: loggedInUser._id}}
+            ]
+        })
+        .select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit)
+
+        res.json({data:users})
     }catch(err){
-        res.status(400).send("something wrong bro"+err.message)
+        res.status(400).send("something wrong bro "+err.message)
     }
 })
 
